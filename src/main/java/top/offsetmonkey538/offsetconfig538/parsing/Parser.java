899f7e7c5b804fcd1fc6d3400d606456ee4686bd
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-06-14 23:31:32 +0300

    Add a private parseValue method to the Parser class. Will be used for parsing a key-value pair into an Object.

--- src/main/java/top/offsetmonkey538/offsetconfig538/parsing/Parser.java
+++ src/main/java/top/offsetmonkey538/offsetconfig538/parsing/Parser.java
@@ -63,6 +63,51 @@ public class Parser {
         return entries;
     }
 
+    private Object parseValue(String line) throws OffsetConfigException {
+        // The value starts after the equals sign and ends at the end of the line.
+        String valueString = line.substring(
+                line.indexOf(OffsetConfig538.KEY_VALUE_DELIMITER) + 1
+        ).trim();
+
+        // String if value starts and ends with double quotes (").
+        if (valueString.startsWith("\"") && valueString.endsWith("\"")) return valueString.substring(1, valueString.length() - 1);
+        // Integer if value is a number without a decimal point.
+        if (valueString.matches("\\d+")) return Integer.parseInt(valueString);
+        // Float if value is a number with a decimal point.
+        if (valueString.matches("\\d+\\.\\d+")) return Float.parseFloat(valueString);
+        // Boolean true if value is "true".
+        if (valueString.equalsIgnoreCase("true")) return true;
+        // Boolean false if value is "false".
+        if (valueString.equalsIgnoreCase("false")) return false;
+
+
+        // Objects and arrays need to define their type.
+        if (valueString.startsWith(OffsetConfig538.TYPE_PREFIX)) {
+            // Type starts after the type prefix.
+            int typeStart = valueString.indexOf(OffsetConfig538.TYPE_PREFIX) + 1;
+            // and ends before the last character which is either
+            // a key-value delimiter or a block start indicator.
+            int typeEnd = valueString.length() - 1;
+
+            String type = valueString.substring(typeStart, typeEnd).trim();
+
+            // Array if value ends with array open.
+            if (valueString.endsWith(OffsetConfig538.ARRAY_OPEN)) return parseArray(type);
+
+            // Object if value ends with object open.
+            if (valueString.endsWith(OffsetConfig538.OBJECT_OPEN)) return parseObject(type);
+
+            throw new OffsetConfigException("Expected '%s' or '%s' at the end of '%s' at line '%s'!",
+                    OffsetConfig538.KEY_VALUE_DELIMITER,
+                    OffsetConfig538.BLOCK_START_INDICATOR,
+                    valueString,
+                    currentLineNumber
+            );
+        }
+
+        throw new OffsetConfigException("Invalid value '%s' at line '%s'!", valueString, currentLineNumber);
+    }
+
     private int getIndentation(String line) {
         int amountOfSpaces = line.length() - line.stripLeading().length();
         return amountOfSpaces / OffsetConfig538.INDENTATION_SIZE;
