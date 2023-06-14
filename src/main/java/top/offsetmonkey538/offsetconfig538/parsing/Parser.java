7bd7646e81a926251327773f16cd03d6cc33e1f9
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-06-14 23:29:24 +0300

    Add Parser class, used for parsing the config format. Has a parse method which will be for parsing the content of a config file into a Map of a String key and Object value.

---
+++ src/main/java/top/offsetmonkey538/offsetconfig538/parsing/Parser.java
@@ -0,0 +1,86 @@
+package top.offsetmonkey538.offsetconfig538.parsing;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Stack;
+import top.offsetmonkey538.offsetconfig538.OffsetConfig538;
+import top.offsetmonkey538.offsetconfig538.exception.OffsetConfigException;
+
+public class Parser {
+    private int currentLineNumber;
+    private String[] lines;
+
+    private final OffsetConfig538 offsetConfig538;
+
+    public Parser(OffsetConfig538 offsetConfig538) {
+        this.offsetConfig538 = offsetConfig538;
+    }
+
+    public Map<String, Object> parse(String content) throws OffsetConfigException {
+        final Map<String, Object> entries = new LinkedHashMap<>();
+
+        // Split content at CR/LF (Windows line-ending) or LF (Unix line-ending).
+        this.lines = content.split("\\r?\\n");
+
+        // Store the current parent in a stack.
+        final Stack<String> parentStack = new Stack<>();
+
+        for (currentLineNumber = 0; currentLineNumber < lines.length; currentLineNumber++) {
+            String line = lines[currentLineNumber];
+            String trimmedLine = line.trim();
+
+            // Skip empty lines and comments.
+            if (trimmedLine.isEmpty() || trimmedLine.startsWith(OffsetConfig538.COMMENT_PREFIX)) {
+                continue;
+            }
+
+            // Get our indentation level
+            final int indentLevel = getIndentation(line);
+
+            // If we are on a lower level of indentation from our parent,
+            // then it isn't our parent.
+            while (!parentStack.isEmpty() && indentLevel < parentStack.size()) {
+                parentStack.pop();
+            }
+
+            // Get our key
+            String key = getKey(line);
+            if (!parentStack.isEmpty()) {
+                key = parentStack.peek() + "." + key;
+            }
+
+            // Check if we are a parent and add ourselves to the parent stack.
+            if (trimmedLine.endsWith(OffsetConfig538.BLOCK_START_INDICATOR)) {
+                parentStack.push(key);
+                continue;
+            }
+
+            // Parse the value and add it to the entries map
+            Object value = parseValue(line);
+            entries.put(key, value);
+        }
+
+        return entries;
+    }
+
+    private int getIndentation(String line) {
+        int amountOfSpaces = line.length() - line.stripLeading().length();
+        return amountOfSpaces / OffsetConfig538.INDENTATION_SIZE;
+    }
+
+    private String getKey(String line) throws OffsetConfigException {
+        // The end of the key is either a key-value
+        // delimiter or a block start indicator.
+        int keyEnd = line.indexOf(OffsetConfig538.KEY_VALUE_DELIMITER);
+        if (keyEnd == -1) keyEnd = line.indexOf(OffsetConfig538.BLOCK_START_INDICATOR);
+
+        if (keyEnd == -1)
+            throw new OffsetConfigException("Expected '%s' or '%s' in '%s' at line '%s'!", OffsetConfig538.KEY_VALUE_DELIMITER, OffsetConfig538.BLOCK_START_INDICATOR, line, currentLineNumber);
+
+        String key = line.substring(0, keyEnd).trim();
+        if (key.equals(""))
+            throw new OffsetConfigException("Expected key in '%s' at line '%s'!", line, currentLineNumber);
+
+        return key;
+    }
+}
